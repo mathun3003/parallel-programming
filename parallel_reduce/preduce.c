ebbaf1199f3eb5dd54cafdb8eb97222eb379e15f
Mathis <mathun3003@gmail.com>
2024-05-13 13:44:13 +0200

    implemented parallel reduce

--- parallel_reduce/preduce.c
+++ parallel_reduce/preduce.c
@@ -2,6 +2,16 @@
 
 #include <pthread.h>
 
+#define NUM_THREADS (5)
+
+struct ThreadData {
+    int (*op)(int, int);
+    int *data;
+    int len;
+    int result;
+};
+
+
 int max(int a, int b)
 {
   return (a>b) ? a : b;
@@ -23,22 +33,57 @@ int reduce(int (*op)(int, int),
   return result;
 }
 
-int parallel_reduce(int (*op)(int, int),
-                    int *data,
-                    int len)
+void *thread_reduce(void *threadArg) {
+    struct ThreadData *data = (struct ThreadData *)threadArg;
+    // reuse sequential reduce implementation
+    data->result = reduce(data->op, data->data, data->len);
+    pthread_exit(NULL);
+}
+
+int parallel_reduce(int (*op)(int, int), 
+                    int *data, 
+                    int len) 
 {
+    pthread_t threads[NUM_THREADS];  // generate identifier for each thread
+    struct ThreadData threadData[NUM_THREADS];  // generate ThreadData for each Thread
+    int i;
+
+    /* 
+    Split data among threads
+    Each Thread obtains its own sub array as parameter
+    */
+    int chunkSize = len / NUM_THREADS;
+    for (i = 0; i < NUM_THREADS; ++i) {
+        threadData[i].op = op;
+        threadData[i].data = data + i * chunkSize;
+        threadData[i].len = (i == NUM_THREADS - 1) ? (len - i * chunkSize) : chunkSize;
+        // create thread
+        pthread_create(&threads[i], NULL, thread_reduce, (void *)&threadData[i]);
+    }
+
+    // wait on termination of all threads
+    for (i = 0; i < NUM_THREADS; ++i)
+        pthread_join(threads[i], NULL);
+
+    // concatenate results
+    int result = threadData[0].result;
+    for (i = 1; i < NUM_THREADS; ++i)
+        result = op(result, threadData[i].result);
+    
+    return result;
 }
 
 int main() {
   int data[] = {1,2,3,4,5,6,7,8,9,10};
+  int arr_len = *(&data + 1) - data;
 
   int m  = reduce(max, data, 10);
   int s = reduce(sum, data, 10);
 
   printf("max : %i; sum: %i\n", m, s);
 
-  int pm = parallel_reduce(max, data, 10);
-  int ps = parallel_reduce(sum, data, 10);
+  int pm = parallel_reduce(max, data, arr_len);
+  int ps = parallel_reduce(sum, data, arr_len);
 
   printf("parallel max : %i; parallel sum: %i\n", pm, ps);
   return 0;
